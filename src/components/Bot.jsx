1f98e981bba7e5fa50adac9ff0da8cbc41e7e3b9
Sachin Kumar <sachinmandal726@gmail.com>
2024-09-09 20:58:01 +0530

    solved

--- src/components/Bot.jsx
+++ src/components/Bot.jsx
@@ -13,7 +13,8 @@ const Bot = () => {
       href={whatsappUrl}
       target="_blank"
       rel="noopener noreferrer"
-      className="fixed bottom-5 sm:right-8 right-4 z-[999] cursor-pointer text-white text-4xl bg-cyan-600 w-16 h-16 flex items-center justify-center rounded-full animate-bounce"
+      className="fixed bottom-5 sm:right-8 right-4 z-[999] cursor-pointer text-white text-4xl 
+      bg-cyan-600 w-16 h-16 flex items-center justify-center rounded-full animate-bounce"
     >
       <ion-icon name="chatbubble-ellipses"></ion-icon>
     </a>
